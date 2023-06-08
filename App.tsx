a5bb1485e91a1bd6b173396fd0aec9e2fc3c23fb
FelipeQuintero <felipe.quintero@utp.edu.co>
2023-06-08 10:34:40 -0500

    Asseting Splash Screen

--- App.tsx
+++ App.tsx
@@ -1,23 +1,80 @@
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-
+import React, {useState,useEffect,useRef} from 'react';
 
 import {
-  SafeAreaView,
+  Image,
+  View,
   StyleSheet,
+  ImageBackground,
+  Animated,
+  SafeAreaView,
 } from 'react-native';
+import { Easing } from 'react-native-reanimated';
 
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-function App(): JSX.Element {
+const SplashScreen = () => (
+  <View style={styles.container}>
+    <Image
+      source={require('./assets/img/ic.png')}
+      style={styles.image}
+    />
+  </View>
+);
+
+const App = () => {
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowSplash(false);
+    }, 2500); 
+  }, []);
+
+  useEffect(() => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 1000, 
+      easing: Easing.linear,
+      useNativeDriver: true,
+    }).start();
+  }, []);
 
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
+      {showSplash ? (
+        <SplashScreen />
+      ):(
+        <ImageBackground
+          source={require('./assets/img/imgBG01.png')}
+          style={[styles.background,{opacity : fadeAnim}]}
+        >
+
+        </ImageBackground>
+      )}
+      
     </SafeAreaView>
+
   );
 }
 
 const styles = StyleSheet.create({
-  
+  container: {
+    flex: 1,
+    backgroundColor :'#FFF'
+  },
+  background:{
+    flex:1,
+    resizeMode: 'cover',
+    justifyContent:'center',
+  },
+  image: {
+    maxHeight: 200,
+    maxWidth: 200,
+    top: '40%',
+    resizeMode: 'contain',
+    alignSelf: 'center'
+  },
 });
 
 export default App;
