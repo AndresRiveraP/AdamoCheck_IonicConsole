3588923dafc7717f48e40a53b5cca6e2431719d9
FelipeQuintero <felipe.quintero@utp.edu.co>
2023-06-13 15:14:55 -0500

    Verified Unchained

--- src/components/Views/Verified.js
+++ src/components/Views/Verified.js
@@ -51,7 +51,7 @@ const styles = StyleSheet.create({
         alignItems : 'center',
     },
     picture:{
-        marginTop: 100,
+        marginTop: 70,
         width: 200,
         height: 200,
         resizeMode: 'cover',
