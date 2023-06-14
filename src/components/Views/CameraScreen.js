5d19db68a942c8f53d510651d585eed7f20cb7e0
FelipeQuintero <felipe.quintero@utp.edu.co>
2023-06-14 16:54:03 -0500

    Camera AT Rivals

--- src/components/Views/CameraScreen.js
+++ src/components/Views/CameraScreen.js
@@ -1,31 +1,42 @@
-import React, { useRef } from 'react';
-import { View, TouchableOpacity, StyleSheet } from 'react-native';
+import React, { useState,useRef } from 'react';
+import { View, TouchableOpacity, StyleSheet,Image} from 'react-native';
 
 import { RNCamera } from 'react-native-camera';
 
 const CameraScreen = ({check}) => {
   const cameraRef = useRef(null);
+  const [cameraView,setCameraView] = useState(true)
+  const [capturedImage, setCapturedImage] = useState(null);
 
   const takePicture = async () => {
     if (cameraRef.current) {
       const options = { quality: 0.5, base64: true };
       const data = await cameraRef.current.takePictureAsync(options);
       console.log(data.uri); // The captured image URI
+      setCapturedImage(data.uri);
+      setCameraView(false)
     }
   };
 
   return (
     <View style={styles.container}>
-      <RNCamera
-        ref={cameraRef}
-        style={styles.camera}
-        type={RNCamera.Constants.Type.front}
-        captureAudio={false}
-      />
-
-      <TouchableOpacity style={styles.button} onPress={takePicture}>
-        {/* Your capture button UI */}
-      </TouchableOpacity>
+      {cameraView ? (
+        <View style={styles.container}>
+          <RNCamera
+            ref={cameraRef}
+            style={styles.camera}
+            type={RNCamera.Constants.Type.front}
+            captureAudio={false}
+          />
+          <TouchableOpacity style={styles.button} onPress={takePicture}>
+            {/* Your capture button UI */}
+          </TouchableOpacity>
+        </View>
+      ):(
+        <View>
+          <Image source={{ uri: capturedImage }} style={{width: '100%', height: '100%',resizeMode:'cover'}} />
+        </View>
+      )}
     </View>
   );
 };
@@ -44,6 +55,13 @@ const styles = StyleSheet.create({
     padding: 25,
     borderRadius: 32,
     backgroundColor: 'white',
+  },
+  image: {
+    width: 200,
+    height: 200,
+    resizeMode: 'cover',
+    bottom: '30%',
+    left: '25%'
   },
 });
 
