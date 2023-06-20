6318a3143d845278c5d27cbf8264093a2e52bf15
FelipeQuintero <felipe.quintero@utp.edu.co>
2023-06-20 12:19:04 -0500

    AdminLog with Modal Loading

--- src/components/Views/AdminLog.js
+++ src/components/Views/AdminLog.js
@@ -3,12 +3,15 @@ import axios from 'axios';
 import {SafeAreaView, Alert,TextInput, Image, StyleSheet, View,Text, KeyboardAvoidingView,Keyboard, TouchableWithoutFeedback,TouchableOpacity, Modal} from 'react-native'
 
 import AdminScreen from './AdminScreen';
+import LoadingModal from './LoadingModal';
 
 const AdminLog = () => {
   const [id,setId] = useState('')
   const [password, setPassword] = useState('')
   const [validLog, setValidLog] = useState(false)
   const [buttonOpacity, setButtonOpacity] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+
 
   const validateForm = () =>{
     if(id == '' || password == ''){
@@ -21,9 +24,12 @@ const AdminLog = () => {
     setButtonOpacity(1);
     if(!validateForm()){
         Alert.alert('Atención', 'Debe completar todos los campos')
-        return 0;
+        return;
     }
+
     try {
+        setIsLoading(true)
+
         var url = 'http://192.168.0.201:3000/admins';
         //json-server --host 192.168.0.201 proofdb.json
         const result = await axios.get(url)
@@ -43,6 +49,11 @@ const AdminLog = () => {
         else{
             console.log("Admin User Not Found")
         }
+
+        setTimeout(() =>{
+            setIsLoading(false)
+        },3000);
+
     } catch (error) {
         console.log(error)
     }
@@ -102,6 +113,15 @@ const AdminLog = () => {
                 </View>
             </View>
         </KeyboardAvoidingView>
+
+        {isLoading && ( 
+            <Modal
+                animationType='fade'
+                style={styles.modalLoading}
+            >
+                <LoadingModal/>
+           </Modal>
+        )}
 
         {validLog && (
             <Modal
@@ -169,6 +189,11 @@ const styles = StyleSheet.create({
     label:{
         alignSelf: 'center',
     },
+    modalLoading:{
+        flex:1,
+        zIndex: 2,
+        justifyContent: 'center',
+      },
 })
 
 export default AdminLog
