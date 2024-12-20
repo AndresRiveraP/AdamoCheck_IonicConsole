57c0afa59d2fa089082d620e046e917f1b856658
Rafael Valencia <rafael.valencia@holdingsbpo.com>
2024-12-20 09:04:22 -0500

    Finished showSplash

--- App.tsx
+++ App.tsx
@@ -12,11 +12,22 @@ import {
   SafeAreaView,
   Dimensions,
   Text,
+  ImageBackground,
 } from 'react-native';
 
 import Navigator from './src/components/Views/Navigator';
 
-const {width} = Dimensions.get('window');
+const {width, height} = Dimensions.get('window');
+const guidelineBaseWidth = 350;
+const guidelineBaseHeight = 680;
+
+const scaleWidthSize = (size: number) => {
+  return (width / guidelineBaseWidth) * size;
+};
+
+const scaleHeightSize = (size: number) => {
+  return (height / guidelineBaseHeight) * size;
+};
 
 const scaleFontSize = (size: number) => {
   const guidelineBaseWidth = 350;
@@ -37,22 +48,15 @@ const SplashScreen = () => (
         top: '-10%',
       }}>
       <Text
-        style={{
-          fontFamily: 'Guitar-Acoustic',
-          fontSize: scaleFontSize(50),
-          color: '#fff',
-        }}>
+        style={[
+          styles.title,
+          {
+            color: '#fff',
+          },
+        ]}>
         adamo
       </Text>
-      <Text
-        style={{
-          fontFamily: 'Guitar-Acoustic',
-          fontSize: scaleFontSize(50),
-          color: '#fff',
-          opacity: 0.4,
-        }}>
-        check
-      </Text>
+      <Text style={[styles.title, {color: '#2bbfed'}]}>check</Text>
     </View>
   </View>
 );
@@ -64,7 +68,7 @@ const App = () => {
   useEffect(() => {
     const fadeOut = Animated.timing(fadeAnim, {
       toValue: 0,
-      duration: 30000,
+      duration: 3000,
       useNativeDriver: true,
     });
 
@@ -97,19 +101,29 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1, //DON'T TOUCH
-    backgroundColor: '#1f1f1f',
+    backgroundColor: '1f1f1f',
     display: 'flex',
     flexDirection: 'column',
     alignContent: 'center',
     justifyContent: 'center',
   },
+  background: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'space-around',
+  },
   image: {
     top: '-10%',
-    width: '40%',
-    height: '40%',
+    width: scaleWidthSize(150),
+    height: scaleHeightSize(150),
     resizeMode: 'contain',
     alignSelf: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    fontFamily: 'Guitar-Acoustic',
+    fontSize: scaleFontSize(50),
+    top: '-10%',
   },
 });
 
