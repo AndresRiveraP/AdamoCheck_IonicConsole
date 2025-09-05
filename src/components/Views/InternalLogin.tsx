fba8926e7f250a887a07cf6ed2917cac0da5b88a
AndresR <andres.rivera@holdingsbpo.com>
2025-09-05 17:00:53 -0500

    cambio de la pantalla de internal login del antiguo al nuevo diseño

--- src/components/Views/InternalLogin.tsx
+++ src/components/Views/InternalLogin.tsx
@@ -63,103 +63,101 @@ const InternalLogin = ({ navigation }: { navigation: NavigationProp<any> }) => {
         <ImageBackground source={require('../../assets/img/backgroundStaff.png')} style={styles.background}>
             <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
                 <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={{flex: 1}}>
-                <View style={[styles.container,  {paddingTop: '10%'}]}>
-                    <View style={[{flexDirection: 'column', alignItems:'center', paddingBottom:'5%'}]}>
+                <View style={[styles.container]}>
+                    <View style={[{ alignItems:'center', marginBottom:'5%', zIndex: 2, top:'-30%'}]}>
                         <Image
-                        source={require('../../assets/img/logoCheck.png')}
+                        source={require('../../assets/img/logo.png')}
                         style={[styles.image, {resizeMode: 'contain'}]}
                         />
                         <View
                             style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center', paddingTop:'10%'
                             }}>
-                            <Text style={[styles.title,{color: '#fff'} ]}>
+                            <Text 
+                              style={[styles.title,{color: '#fff'} ]}>
                                 adamo
                             </Text>
-                            <Text style={[styles.title, {color: '#c3e5f3'}]}>check</Text>
+                            <Text 
+                              style={[styles.title, {color: '#bcc988'}]}>
+                              check
+                              </Text>
                         </View>
                     </View>
 
-                    <View style={{alignItems: 'center', top: '-10%' }}>
-                        <Text style={[{color: '#fff', fontSize: scaleFontSize(18)}]}>
-                            Internal <Text style={[{fontWeight: 'bold'}]}>login</Text>
-                        </Text>
-                        <Text style={[{color: '#fff', fontSize: scaleFontSize(18)}]}>
-                            <Text style={[{fontWeight: 'bold'}]}>Staff</Text> credentials required.
-                        </Text>
+                    <View style={[styles.containerContent, {backgroundColor: '#EBF3CB'}]}>
+                        <View style={{alignItems: 'center', marginTop: '10%'}}>
+                            <Text style={[{color: '#78910F', fontSize: scaleFontSize(55), fontFamily:'Sora-ExtraBold'}]}>
+                                Hello!
+                            </Text>
+                            <Text style={styles.subtitileHello}>
+                                Ready to start?
+                            </Text>
+                        </View>
+                        <View style={styles.containerForm}>
+                            <View style={styles.inputContainer}>
+                                <Image
+                                source={require('../../assets/img/user.png')} 
+                                style={[styles.icon, {resizeMode: "contain"}]}
+                                />
+
+                                    {user.length === 0 && (
+                                        <Text style={styles.fakePlaceholder}>Username</Text>
+                                    )}
+                                    <TextInput
+                                        value={user}
+                                        onChangeText={setUser}
+                                        style={styles.input}
+                                        placeholder=""  
+                                        textAlign="left"
+                                    />
+                            </View>
+
+                            <View style={styles.inputContainer}>                                
+                                <Image
+                                source={require('../../assets/img/password.png')} 
+                                style={[styles.icon, {resizeMode: "contain"}]}
+                                />
+                                    {password.length === 0 && (
+                                        <Text style={styles.fakePlaceholder}>Password</Text>
+                                    )}
+                                    <TextInput
+                                        value={password}
+                                        onChangeText={setPassword}
+                                        secureTextEntry={true}                                
+                                        style={styles.input}
+                                        placeholder=""  
+                                        textAlign="left"
+                                    />
+                            </View>
+                          </View>
+                            <Pressable
+                                onPress={() => handleLogin()}
+                                style={({ pressed }) => ([
+                                    {
+                                    width: scaleWidthSize(150),
+                                    height: scaleHeightSize(45), 
+                                    borderRadius: 40,
+                                    overflow: 'hidden',      
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                    backgroundColor: '#9EBE1A',
+                                    top: '5%'
+                                    },
+                                    pressed && { opacity: 0.7 } 
+                                ])}
+                                
+                                android_ripple={{ color: 'rgba(255, 255, 255, 0.1)', borderless: false }}
+                                hitSlop={{ top: 10, right: 10, bottom: 10, left: 10 }}
+                                accessibilityRole="button"
+                                accessibilityLabel="Abrir perfil"
+                              >
+                              <Text style={[{color: '#fff', fontSize: scaleFontSize(15), fontFamily:'Sora-ExtraBold'}]}>LOGIN</Text>
+                            </Pressable>
+                            <Image
+                                source={require('../../assets/img/adamoByHBPO.png')} 
+                                style={[{resizeMode: "contain", width: scaleWidthSize(120), marginTop: '15%'}]}
+                              />
+                        </View>
                     </View>
-                        <Image 
-                        source={require('../../assets/img/staff.png')}
-                        style={[styles.image, {top: '-5%', width: scaleWidthSize(60), height: scaleHeightSize(55),}]}
-                        />
-                    
-                        <View style={styles.inputContainer}>
-                            <Image
-                            source={require('../../assets/img/user.png')} 
-                            style={[styles.icon, {resizeMode: "contain"}]}
-                            />
-
-                                {user.length === 0 && (
-                                    <Text style={styles.fakePlaceholder}>Username</Text>
-                                )}
-                                <TextInput
-                                    value={user}
-                                    onChangeText={setUser}
-                                    style={styles.input}
-                                    placeholder=""  
-                                    textAlign="left"
-                                />
-                        </View>
-                    
-                    
-                        <View style={styles.inputContainer}>
-                            
-                            <Image
-                            source={require('../../assets/img/locker.png')} 
-                            style={[styles.icon, {resizeMode: "contain"}]}
-                            />
-
-                                {password.length === 0 && (
-                                    <Text style={styles.fakePlaceholder}>Password</Text>
-                                )}
-                                <TextInput
-                                    value={password}
-                                    onChangeText={setPassword}
-                                    secureTextEntry={true}                                
-                                    style={styles.input}
-                                    placeholder=""  
-                                    textAlign="left"
-                                />
-                        </View>
-                    
-                        <Pressable
-                            onPress={() => handleLogin()}
-                            style={({ pressed }) => ([
-                                {
-                                width: '15%',            
-                                aspectRatio: 1,          
-                                borderRadius: 999,       
-                                overflow: 'hidden',      
-                                alignItems: 'center',
-                                justifyContent: 'center',
-                                backgroundColor: 'transparent',
-                                top: '4%',
-                                left: '30%'
-                                },
-                                pressed && { opacity: 0.7 } 
-                            ])}
-                            
-                            android_ripple={{ color: 'rgba(255, 255, 255, 0.1)', borderless: false }}
-                            hitSlop={{ top: 10, right: 10, bottom: 10, left: 10 }}
-                            accessibilityRole="button"
-                            accessibilityLabel="Abrir perfil"
-                        >
-                        <Image
-                            source={require('../../assets/img/arroyLogin.png')}
-                            style={{ width: '100%', height: '100%' }}
-                            resizeMode="contain"      
-                        />
-                        </Pressable>
-                </View>
             </KeyboardAvoidingView>
           </TouchableWithoutFeedback>
         </ImageBackground>
@@ -171,60 +169,77 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-    },
+        display: 'flex',
+        marginTop: '10%',
+      },
     background: {
         flex: 1,
         resizeMode: 'cover',
         justifyContent: 'space-around',
     },
+    containerContent: {
+        position: 'absolute',
+        top: '25%',
+        bottom: '-100%',
+        backgroundColor: '#EBF3CB',
+        borderRadius: 40,
+        display: 'flex',
+        alignItems: 'center'
+    },
     image: {
-        top: '-10%',
         width: scaleWidthSize(80),
         height: scaleHeightSize(70),
-        marginBottom: '5%',
+        marginBottom: '10%',
     },
     title: {
         fontFamily: 'Guitar-Acoustic',
-        fontSize: scaleFontSize(50),
-        top: '-20%',  
+        fontSize: scaleFontSize(40),
+        top: '-22%',
     },
-    wrapper: {
-        width: scaleWidthSize(240),
-        alignSelf: 'center',
+    subtitileHello: {
+      fontFamily: 'inter',
+      color: '#78910F',
+      fontSize: scaleFontSize(14),
+      fontWeight:'normal',
+      letterSpacing: 5,
+      marginTop: '-3%',
+    },
+    containerForm: {
+        marginTop: '10%',
     },
     icon: {
-        width: scaleWidthSize(20),
-        height: scaleHeightSize(18),
+        width: scaleWidthSize(24),
+        height: scaleHeightSize(22),
         marginRight: "5%",
         marginLeft: '2%',
     },
     inputContainer: {
-        width: scaleWidthSize(250),
+        width: scaleWidthSize(200),
         height: scaleHeightSize(45),
         alignSelf: 'center',
         position: 'relative',        
         flexDirection: 'row',
         alignItems: 'center',
-        backgroundColor: '#c1e7f4',
+        backgroundColor: '#EBF3CB',
         borderRadius: 50,
         borderWidth: 1,
-        borderColor: '#FFFFFF',
+        borderColor: '#4A5714',
         marginTop: '5%',
-        paddingVertical: '1%',
         paddingHorizontal: '4%',
+        marginHorizontal: '10%',
     },
     fakePlaceholder: {
         position: 'absolute',        
         left: 0,
         right: 0,
         textAlign: 'center',         
-        color: '#FFFFFF',           
+        color: '#4C4C4C',           
         fontSize: scaleFontSize(15),                
         opacity: 0.8,                
     },
     input: {
         flex: 1,
-        color: '#FFFFFF',
+        color: '#4C4C4C',
         fontSize: scaleFontSize(15),
         textAlign: 'left',
     },
