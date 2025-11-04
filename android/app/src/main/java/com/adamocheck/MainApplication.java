83d64e1cbb5893aa7c130929fe43f0b2f896382f
AndresR <andres.rivera@holdingsbpo.com>
2025-11-04 17:12:22 -0500

    Release funcionando

--- android/app/src/main/java/com/adamocheck/MainApplication.java
+++ android/app/src/main/java/com/adamocheck/MainApplication.java
@@ -1,21 +1,21 @@
 package com.adamocheck;
 
 import android.app.Application;
-import com.facebook.react.PackageList;
+import android.content.Context;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
-import com.facebook.react.ReactInstanceManager;
-import org.reactnative.camera.RNCameraPackage;
+
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
       new DefaultReactNativeHost(this) {
+
         @Override
         public boolean getUseDeveloperSupport() {
           return BuildConfig.DEBUG;
@@ -23,11 +23,8 @@ public class MainApplication extends Application implements ReactApplication {
 
         @Override
         protected List<ReactPackage> getPackages() {
-          @SuppressWarnings("UnnecessaryLocalVariable")
-          List<ReactPackage> packages = new PackageList(this).getPackages();
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
-          return packages;
+          // Si tenías paquetes manuales, agrégalos aquí
+          return new java.util.ArrayList<>();
         }
 
         @Override
@@ -36,13 +33,13 @@ public class MainApplication extends Application implements ReactApplication {
         }
 
         @Override
-        protected boolean isNewArchEnabled() {
-          return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+        protected Boolean isHermesEnabled() {
+          return Boolean.TRUE;
         }
 
         @Override
-        protected Boolean isHermesEnabled() {
-          return BuildConfig.IS_HERMES_ENABLED;
+        protected boolean isNewArchEnabled() {
+          return false;
         }
       };
 
@@ -55,10 +52,8 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
+    if (mReactNativeHost.getReactInstanceManager().getDevSupportManager() != null) {
       DefaultNewArchitectureEntryPoint.load();
     }
-    //ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
 }
